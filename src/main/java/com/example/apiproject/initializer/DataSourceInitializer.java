8bda1c0867c1dc7b146c82debdb9895319d6772c
ZhangzeqingCN <1804924578@qq.com>
2023-05-27 17:01:53 +0800

    doc and fix

--- src/main/java/com/example/apiproject/initializer/DataSourceInitializer.java
+++ src/main/java/com/example/apiproject/initializer/DataSourceInitializer.java
@@ -1,6 +1,8 @@
 package com.example.apiproject.initializer;
 
+import com.example.apiproject.access.Commodity;
 import com.example.apiproject.access.User;
+import com.example.apiproject.repository.CommodityRepository;
 import com.example.apiproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,15 +16,26 @@ public class DataSourceInitializer implements CommandLineRunner {
 
     private UserRepository userRepository;
 
+
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    private CommodityRepository commodityRepository;
+
+    @Autowired
+    public void setCommodityRepository(CommodityRepository commodityRepository) {
+        this.commodityRepository = commodityRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         userRepository.save(User.builder().name("user").password("61259cdf-9cb1-4981-b926-35ebe0906c29").build());
         userRepository.save(User.builder().name("user1").password("123").build());
+        userRepository.save(User.builder().name("zzq").password("123").build());
+
+        commodityRepository.save(Commodity.builder().price(0.2F).name("Test").build());
     }
 }
 
