e9b0f0f42ccbd26f10fc4ced6251627799997acd
ZhangzeqingCN <1804924578@qq.com>
2023-05-29 16:03:38 +0800

    test

---
+++ src/main/java/com/example/apiproject/controller/MyErrorController.java
@@ -0,0 +1,14 @@
+package com.example.apiproject.controller;
+
+import com.example.apiproject.domain.Result;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public Result error() {
+        return Result.error("error from " + getClass().getName());
+    }
+}
