4073959da8e0f59293f7ee37874b4e55891e8ffa
ZhangzeqingCN <1804924578@qq.com>
2023-06-10 11:44:36 +0800

    拦截器对openApi放行

--- src/main/java/com/example/apiproject/configuration/MyInterceptorConfiguration.java
+++ src/main/java/com/example/apiproject/configuration/MyInterceptorConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.apiproject.configuration;
 
 import com.example.apiproject.interceptor.AuthInterceptor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,10 +17,22 @@ public class MyInterceptorConfiguration implements WebMvcConfigurer {
     }
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(@NotNull InterceptorRegistry registry) {
         registry
                 .addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/auth/**", "/dev/**", "/css/**", "/favicon.ico","/doc.html");
+                .excludePathPatterns(
+                        "/auth/**",
+                        "/dev/**",
+                        "/css/**",
+                        "/favicon.ico",
+                        "/doc.html",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/apiproject/swagger-ui.html",
+                        "/doc.html",
+                        "/doc.html#/**",
+                        "/webjars/**"
+                );
     }
 }
